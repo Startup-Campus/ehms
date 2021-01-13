2c3a4b3904bdf8c5fe65bcbd54dd73b723405da2
unknown <obriggs03@gmail.com>
2021-01-13 19:49:33 +0100

    DB constants changed

--- app/config/config.php
+++ app/config/config.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'us-cdbr-east-03.cleardb.com');
 define('DB_USER', 'b26a3381db0c45');
 define('DB_PASS', '2c227266');
-define('DB_NAME', 'ehms');
+define('DB_NAME', 'heroku_f4c5865480229ec');
 
 // APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));

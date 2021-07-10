a592aaa25ab3ccbc0e63f8abcc3cbb38d0f64ce5
Tamunokorite Briggs <obriggs03@gmail.com>
2021-07-10 01:58:43 +0100

    Added cleardb config

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,11 @@
 <?php 
 
 // Database parameters
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'ehms');
+$cleardb_url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+define('DB_HOST', $cleardb_url['host']);
+define('DB_USER', $cleardb_url['user']);
+define('DB_PASS', $cleardb_url['pass']);
+define('DB_NAME', substr($cleardb_url["path"],1));
 
 // APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));

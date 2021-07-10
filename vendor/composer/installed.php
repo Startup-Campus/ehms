9c02bbaf83aa7808aacc1f0e003f9b9c13f51f16
Tamunokorite Briggs <obriggs03@gmail.com>
2021-07-10 01:50:38 +0100

    added lock file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f39acc7c7011076993cac33ef0085c3fdaebf8bb',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f39acc7c7011076993cac33ef0085c3fdaebf8bb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
